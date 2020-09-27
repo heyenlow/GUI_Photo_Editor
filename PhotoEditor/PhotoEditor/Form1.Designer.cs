ed60b18fd055d14b796c630e94fdfb5ebf2b5da3
LukeTreece-HU <71277076+LukeTreece-HU@users.noreply.github.com>
2020-09-27 14:13:19 -0500

    Added listing of filenames in Resource directory
    Procrastination Penalty #1 averted

--- PhotoEditor/PhotoEditor/Form1.Designer.cs
+++ PhotoEditor/PhotoEditor/Form1.Designer.cs
@@ -28,13 +28,46 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(13, 13);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(118, 225);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(137, 13);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(267, 225);
+            this.listBox1.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(431, 387);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ListBox listBox1;
     }
 }
 
--- PhotoEditor/PhotoEditor/Form1.cs
+++ PhotoEditor/PhotoEditor/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,76 @@ namespace PhotoEditor
         public Form1()
         {
             InitializeComponent();
+
+            // Create three items with sets of subitems for each item
+
+            ListViewItem item1 = new ListViewItem("item1", 0);   // Text and image index
+            item1.SubItems.Add("1");   // Column 2
+            item1.SubItems.Add("2");   // Column 3
+            item1.SubItems.Add("3");   // Column 4
+
+            ListViewItem item2 = new ListViewItem("item2", 1);
+            item2.SubItems.Add("4");
+            item2.SubItems.Add("5");
+            item2.SubItems.Add("6");
+
+            ListViewItem item3 = new ListViewItem("item3", 2);
+            item3.SubItems.Add("7");
+            item3.SubItems.Add("8");
+            item3.SubItems.Add("9");
+
+            // Create columns (Width of -2 indicates auto-size)
+            listView1.Columns.Add("Column 1", -2, HorizontalAlignment.Left);
+            listView1.Columns.Add("Column 2", -2, HorizontalAlignment.Left);
+            listView1.Columns.Add("Column 3", 40, HorizontalAlignment.Right);
+            listView1.Columns.Add("Column 4", 40, HorizontalAlignment.Center);
+
+            // Add the items to the list view
+            listView1.Items.AddRange(new ListViewItem[] { item1, item2, item3 });
+
+            // Show default view
+            //listView1.View = View.Details;
+            listView1.View = View.LargeIcon;
+
+            ScanDirectory();
+        }
+
+        private void ScanDirectory()
+        {
+            // ! this must change !
+            DirectoryInfo homeDir = new DirectoryInfo(AppDomain.CurrentDomain.BaseDirectory);
+            DirectoryInfo resDir = new DirectoryInfo(Path.Combine(homeDir.FullName, "Resources"));
+
+            Console.WriteLine("Resource Directory: " + resDir);
+            //ListBox myImages = new ListBox();
+            foreach (FileInfo file in resDir.GetFiles("*.jpg"))
+            {
+                try
+                {
+                    byte[] bytes = System.IO.File.ReadAllBytes(file.FullName);
+                    MemoryStream ms = new MemoryStream(bytes);
+                    Image img = Image.FromStream(ms); // Don’t use Image.FromFile() !!!
+                    listBox1.Items.Add(file.Name);
+                    Console.WriteLine("Filename: " + file.Name);
+                    Console.WriteLine("Last mod: " + file.LastWriteTime.ToString());
+                    Console.WriteLine("File size: " + file.Length);
+                }
+                catch
+                {
+                    Console.WriteLine("This is not an image file");
+                }
+            }
+            //listBox1.Items = myImages.Items;
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
