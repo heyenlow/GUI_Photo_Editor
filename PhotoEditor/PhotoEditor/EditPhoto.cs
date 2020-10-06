9c5c951c25638baa967b496fca0acc241dbddd0e
heyenlow <57604869+heyenlow@users.noreply.github.com>
2020-10-06 10:14:38 -0500

    Threading
    Brightness and Color

--- PhotoEditor/PhotoEditor/EditPhoto.cs
+++ PhotoEditor/PhotoEditor/EditPhoto.cs
@@ -20,7 +20,6 @@ namespace PhotoEditor
         {
             await Task.Run(()=>
             {
-                Thread.Sleep(5000);
                 int i = 0;
                 for (int y = 0; y < transformedBitmap.Height; y++)
                 {
@@ -37,41 +36,47 @@ namespace PhotoEditor
                 }
             });
         }
-        private void AlterColors(Color chosenColor)
+        private async Task AlterColors(Color chosenColor)
         {
-            for (int y = 0; y < transformedBitmap.Height; y++)
+            await Task.Run(() =>
             {
-                for (int x = 0; x < transformedBitmap.Width; x++)
-                    //6
+                for (int y = 0; y < transformedBitmap.Height; y++)
                 {
-                    var color = transformedBitmap.GetPixel(x, y);
-                    int ave = (color.R + color.G + color.B) / 3;
-                    double percent = ave / 255.0;
-                    int newRed = Convert.ToInt32(chosenColor.R * percent);
-                    int newGreen = Convert.ToInt32(chosenColor.G * percent);
-                    int newBlue = Convert.ToInt32(chosenColor.B * percent);
-                    var newColor = Color.FromArgb(newRed, newGreen, newBlue);
-                    transformedBitmap.SetPixel(x, y, newColor);
+                    for (int x = 0; x < transformedBitmap.Width; x++)
+                    //6
+                    {
+                        var color = transformedBitmap.GetPixel(x, y);
+                        int ave = (color.R + color.G + color.B) / 3;
+                        double percent = ave / 255.0;
+                        int newRed = Convert.ToInt32(chosenColor.R * percent);
+                        int newGreen = Convert.ToInt32(chosenColor.G * percent);
+                        int newBlue = Convert.ToInt32(chosenColor.B * percent);
+                        var newColor = Color.FromArgb(newRed, newGreen, newBlue);
+                        transformedBitmap.SetPixel(x, y, newColor);
+                    }
                 }
-            }
+            });
         }
         // brightness is a value between 0 – 100. Values < 50 makes the image darker, > 50 makes lighter
-        private void ChangeBrightness(int brightness)
+        private async Task ChangeBrightness(int brightness)
         {
-            // Calculate amount to change RGB
-            int amount = Convert.ToInt32(2 * (50 - brightness) * 0.01 * 255);
-            for (int y = 0; y < transformedBitmap.Height; y++)
+            await Task.Run(() =>
             {
-                for (int x = 0; x < transformedBitmap.Width; x++)
+                // Calculate amount to change RGB
+                int amount = Convert.ToInt32(2 * (50 - brightness) * 0.01 * 255);
+                for (int y = 0; y < transformedBitmap.Height; y++)
                 {
-                    var color = transformedBitmap.GetPixel(x, y);
-                    int newRed = Math.Max(0, Math.Min(color.R - amount, 255));
-                    int newGreen = Math.Max(0, Math.Min(color.G - amount, 255));
-                    int newBlue = Math.Max(0, Math.Min(color.B - amount, 255));
-                    var newColor = Color.FromArgb(newRed, newGreen, newBlue);
-                    transformedBitmap.SetPixel(x, y, newColor);
+                    for (int x = 0; x < transformedBitmap.Width; x++)
+                    {
+                        var color = transformedBitmap.GetPixel(x, y);
+                        int newRed = Math.Max(0, Math.Min(color.R - amount, 255));
+                        int newGreen = Math.Max(0, Math.Min(color.G - amount, 255));
+                        int newBlue = Math.Max(0, Math.Min(color.B - amount, 255));
+                        var newColor = Color.FromArgb(newRed, newGreen, newBlue);
+                        transformedBitmap.SetPixel(x, y, newColor);
+                    }
                 }
-            }
+            });
         }
 
         public EditPhoto(FileInfo file)
@@ -90,8 +95,9 @@ namespace PhotoEditor
 
         private async void InvertButton_ClickAsync(object sender, EventArgs e)
         {
+            this.Enabled = false;
             await InvertColors();
-            
+            this.Enabled = true;
             this.Refresh();
         }
 
@@ -100,19 +106,19 @@ namespace PhotoEditor
 
         }
 
-        private void ColorButton_Click(object sender, EventArgs e)
+        private async void ColorButton_Click(object sender, EventArgs e)
         {
             ColorDialog ColorDialog1 = new ColorDialog();
             if(ColorDialog1.ShowDialog() == DialogResult.OK)
             {
-                AlterColors(ColorDialog1.Color);
+                await AlterColors(ColorDialog1.Color);
                 this.Refresh();
             }
         }
 
-        private void trackBar1_Scroll(object sender, EventArgs e)
+        private async void trackBar1_Scroll(object sender, EventArgs e)
         {
-            ChangeBrightness(trackBar1.Value);
+            await ChangeBrightness(trackBar1.Value);
             this.Refresh();
         }
 
