a0e6c9f5d566b33ef7516eeb60e9fef19a29a1f9
LukeTreece-HU <71277076+LukeTreece-HU@users.noreply.github.com>
2020-10-07 22:59:16 -0500

    Calm Before The Storm
    Adding in what I've done so far; will need to add more tonight

---
+++ PhotoEditor/PhotoEditor/AboutDialog.Designer.cs
@@ -0,0 +1,105 @@
+﻿namespace PhotoEditor
+{
+    partial class AboutDialog
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.okBtn = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(56, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(278, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Authors: Konrad Heyens & Luke Treece";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(124, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(132, 26);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Photo Editor";
+            // 
+            // okBtn
+            // 
+            this.okBtn.Location = new System.Drawing.Point(129, 158);
+            this.okBtn.Name = "okBtn";
+            this.okBtn.Size = new System.Drawing.Size(118, 23);
+            this.okBtn.TabIndex = 2;
+            this.okBtn.Text = "OK";
+            this.okBtn.UseVisualStyleBackColor = true;
+            this.okBtn.Click += new System.EventHandler(this.okBtn_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(46, 81);
+            this.label3.MaximumSize = new System.Drawing.Size(300, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(300, 60);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "This application allows you to search for images on your computer, edit them, and" +
+    " save your changes to them.";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // AboutDialog
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(386, 193);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.okBtn);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "AboutDialog";
+            this.Text = "About";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button okBtn;
+        private System.Windows.Forms.Label label3;
+    }
+}
